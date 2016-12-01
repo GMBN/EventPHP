d57fefbb0bbfc4cba3293add386fc2f5e9fb8247
Gian Nunes <gian@btitecnologia.com>
2016-12-01 17:57:38 -0200

    add priority

--- src/Event.php
+++ src/Event.php
@@ -1,19 +1,27 @@
 <?php
-namespace EventPHP;
 
 class Event {
 
-    function on($event, $callback = false) {
-        $GLOBALS['e_event'][$event][] = $callback;
+    function on($event, $callback = false, $priority = 100) {
+        while (isset($GLOBALS['e_event'][$event][$priority])) {
+            $priority++;
+        }
+        $GLOBALS['e_event'][$event][$priority] = $callback;
     }
 
     function trigger($event, $param = false) {
-        foreach ($GLOBALS['e_event'][$event] as $callback) {
-            if ($callback) {
-                if (isset($param[0])) {
-                    call_user_func_array($callback, $param);
-                }else{
-                    $callback();
+        if (isset($GLOBALS['e_event'][$event])) {
+            $event = $GLOBALS['e_event'][$event];
+            
+            ksort($event); //Ordena o array pelas chaves
+            
+            foreach ($event as $callback) {
+                if ($callback) {
+                    if (isset($param[0])) {
+                        call_user_func_array($callback, $param);
+                    } else {
+                        $callback();
+                    }
                 }
             }
         }
