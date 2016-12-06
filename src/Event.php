7552fe98e4301dd445d50ffda60b152fc6b65331
Gian Nunes <gian@btitecnologia.com>
2016-12-06 09:30:04 -0200

    add debug

--- src/Event.php
+++ src/Event.php
@@ -1,31 +1,65 @@
 <?php
+
 namespace EventPHP;
 
 class Event {
 
+    protected $event = [];
+    protected $file = [];
+    protected $executed = [];
+
     function on($event, $callback = false, $priority = 100) {
-        while (isset($GLOBALS['e_event'][$event][$priority])) {
+        while (isset($this->event[$event][$priority])) {
             $priority++;
         }
-        $GLOBALS['e_event'][$event][$priority] = $callback;
+        $this->event[$event][$priority] = $callback;
+
+        $this->file[$event][$priority] = $this->getFile($callback);
     }
 
     function trigger($event, $param = false) {
-        if (isset($GLOBALS['e_event'][$event])) {
-            $event = $GLOBALS['e_event'][$event];
-            
-            ksort($event); //Ordena o array pelas chaves
-            
-            foreach ($event as $callback) {
+        if (isset($this->event[$event])) {
+            $listeners = $this->event[$event];
+
+            ksort($listeners); //Ordena o array pelas chaves
+
+            foreach ($listeners as $priority => $callback) {
                 if ($callback) {
                     if (isset($param[0])) {
                         call_user_func_array($callback, $param);
                     } else {
                         $callback();
                     }
+                    $this->executed[][$priority][$event] = $this->getFile($callback);
                 }
             }
         }
+    }
+
+    function listeners($event = false) {
+        if ($event) {
+            if (isset($this->file[$event])) {
+                ksort($this->file[$event]);
+
+                return $this->file[$event];
+            }
+            return false;
+        }
+
+        foreach ($this->file as &$f) {
+            ksort($f);
+        }
+        return $this->file;
+    }
+    
+    function debug(){
+        return $this->executed;
+    }
+
+    private function getFile($callback) {
+        $ref = new \ReflectionFunction($callback);
+        $file = $ref->getFileName();
+        return $file;
     }
 
 }
